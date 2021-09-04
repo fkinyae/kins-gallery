4cb8d6fc8838a5e6502e2802ae0ec7f3f41cb01c
Francis Kinyae <fmkinyae@gmail.com>
2021-09-04 10:03:51 +0300

    add all url changes

--- gallery/urls.py
+++ gallery/urls.py
@@ -5,7 +5,7 @@ from django.conf import settings
 
 urlpatterns = [
     path('', views.home, name='home')
-]
+    ]
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
