af76b1293b52c67d366c16a3b0750db9b7571b8a
Francis Kinyae <fmkinyae@gmail.com>
2021-09-04 10:02:22 +0300

    add location to the home view

--- gallery/views.py
+++ gallery/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from .models import Image
+from .models import Image,Location
 
 def home(request):
     images=Image.get_all_images()
-    return render(request, 'all_gallery/home.html',{"images":images})
+    locations=Location.get_locations()
+
+    return render(request, 'all_gallery/home.html',{"images":images,"locations":locations})
+
+def all_locations(request):
+    locations=Location.get_locations()
+    return render(request,'all_gallery/home.html',{"locations":locations})
