77133fbd4b309c96e082d7f4555731128bd9bb46
Francis Kinyae <fmkinyae@gmail.com>
2021-09-04 13:35:17 +0300

    sort images by location button

--- gallery/views.py
+++ gallery/views.py
@@ -13,5 +13,6 @@ def all_locations(request):
     return render(request,'all_gallery/home.html',{"locations":locations})
 
 def image_by_location(request,location):
+    locations=Location.get_locations()
     images=Image.get_image_by_location(location)
-    return render(request,'all_gallery/image_location.html',{"images":images,"location":location})
+    return render(request,'all_gallery/image_location.html',{"images":images,"location":location,"locations":locations})
