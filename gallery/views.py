3f8190b79fa74c44ac5616589b744c2f1784477a
Francis Kinyae <fmkinyae@gmail.com>
2021-09-04 12:32:11 +0300

    add view for images by location(pass location)n

--- gallery/views.py
+++ gallery/views.py
@@ -11,3 +11,7 @@ def home(request):
 def all_locations(request):
     locations=Location.get_locations()
     return render(request,'all_gallery/home.html',{"locations":locations})
+
+def image_by_location(request,location):
+    images=Image.get_image_by_location(location)
+    return render(request,'all_gallery/image_location.html',{"images":images,"location":location})
