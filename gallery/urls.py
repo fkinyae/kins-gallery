9004d8ce6f315026e23686777570d5bebc3df115
Francis Kinyae <fmkinyae@gmail.com>
2021-09-04 12:32:54 +0300

    add dynamic route for the location images

--- gallery/urls.py
+++ gallery/urls.py
@@ -4,7 +4,8 @@ from django.conf.urls.static import static
 from django.conf import settings
 
 urlpatterns = [
-    path('', views.home, name='home')
+    path('', views.home, name='home'),
+    path('image_location/<location>/',views.image_by_location,name='image_by_location'),
     ]
 
 if settings.DEBUG:
