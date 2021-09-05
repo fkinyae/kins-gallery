c584b47edc3f1828f50c242a828f01b9aa1ab336
Francis Kinyae <fmkinyae@gmail.com>
2021-09-05 21:48:27 +0300

    add url to a single image

--- gallery/urls.py
+++ gallery/urls.py
@@ -6,6 +6,8 @@ from django.conf import settings
 urlpatterns = [
     path('', views.home, name='home'),
     path('image_location/<location>/',views.image_by_location,name='image_by_location'),
+    path('search/',views.search_by_category,name="search"),
+    path('image/<int:image_id>/',views.single_image,name='image')
     ]
 
 if settings.DEBUG:
