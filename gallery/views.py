0f57cdcab7b790064a240fae3dd8a4dab3fce4e7
Francis Kinyae <fmkinyae@gmail.com>
2021-09-05 21:49:07 +0300

    add a search view

--- gallery/views.py
+++ gallery/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-from .models import Image,Location
+from django.http import HttpResponse,Http404
+from .models import Image,Location,Category
 
 def home(request):
     images=Image.get_all_images()
@@ -16,3 +16,25 @@ def image_by_location(request,location):
     locations=Location.get_locations()
     images=Image.get_image_by_location(location)
     return render(request,'all_gallery/image_location.html',{"images":images,"location":location,"locations":locations})
+
+def search_by_category(request):
+    categories=Category.objects.all()
+    locations=Location.objects.all()
+    
+    if 'category' in request.GET and request.GET["category"]:
+        search_term=request.GET.get("category")
+        searched_categories=Image.search_by_category(search_term)
+        message = f"{search_term}"
+        
+        return render(request, 'all_gallery/search.html',{"message":message,"searched_categories":searched_categories,"categories":categories,"locations":locations})
+    
+    else:
+        message ="You haven't searched for any term"
+        return render(request, 'all_gallery/search.html',{"message":message})
+
+def single_image(request,image_id):
+    try:
+        image=Image.objects.get(id=image_id)
+    except Image.DoesNotExist:
+        raise Http404()
+    return render(request,"all_gallery/image.html",{"image":image})    
