2d17ee440e9bf3e6ca9c4d1b2d6bfaa6a1e9ab28
Francis Kinyae <fmkinyae@gmail.com>
2021-09-06 15:17:25 +0300

    changes in the search form

--- gallery/models.py
+++ gallery/models.py
@@ -64,7 +64,7 @@ class Image(models.Model):
     
     @classmethod
     def search_by_category(cls,search_term):
-        images = cls.objects.filter(image_name__icontains=search_term)
+        images = cls.objects.filter(category_id__category__icontains=search_term)
         return images
     
     @classmethod
